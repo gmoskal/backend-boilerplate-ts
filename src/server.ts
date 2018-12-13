6a4a0094b7d846f497dc89856256b02bbede4508
Grzegorz Moskal <g.moskal@gmail.com>
2018-12-13 15:40:02 +0100

    tslint

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
-import { Request, Response } from "express"
-
 import * as express from "express"
 import { calculate } from "./policy"
-const app: express.Application = express()
-const port: number = parseInt(process.env.PORT) || 3000
 
-app.get("/", (_req: Request, res: Response) => {
+const app: express.Application = express()
+const port: number = parseInt(process.env.PORT, 10) || 3000
+
+app.get("/", (_req: express.Request, res: express.Response) => {
     res.send("Hello, World!")
 })
 
-app.get("/:name", (req: Request, res: Response) => {
+app.get("/:name", (req: express.Request, res: express.Response) => {
     const { name } = req.params
     res.send(`Hello, ${calculate(name)}`)
 })
 
 app.listen(port, () => {
+    // tslint:disable-next-line:no-console
     console.log(`Listening at http://localhost:${port}/`)
 })
